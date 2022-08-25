d1153a432114938f11c541d4fa9e50d781397cfb
Beatriz Salles <beatrizsallesss@gmail.com>
2022-08-25 08:34:35 -0300

    feat: Create ct_002 test

--- tests/CT_001.py
+++ tests/CT_001.py
@@ -3,6 +3,7 @@ from login import login
 from utils import driver
 from selenium.webdriver.common.by import By
 
+start_time = time.time()
 # Exibir datas de atividades das disciplinas em curso no calendário
 def CT_001():
     try:
@@ -18,10 +19,12 @@ def CT_001():
     except:
       print('CT_001: ❌ - Exibir datas de atividades das disciplinas em curso no calendário')
 
+execution =( time.time() - start_time) * 1000
 
 if(__name__ == '__main__'):
     CT_001()
     time.sleep(3)
     driver.close()
+    print("Done in", round(execution, 4), "ms." )
 
 
