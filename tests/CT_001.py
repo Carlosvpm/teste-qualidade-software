fdca62cb2441663dc2c86f34b6e22426f88230a7
Beatriz Salles <beatrizsallesss@gmail.com>
2022-08-25 00:51:45 -0300

    feat: Add test results icon

--- tests/CT_001.py
+++ tests/CT_001.py
@@ -5,18 +5,23 @@ from selenium.webdriver.common.by import By
 
 # Exibir datas de atividades das disciplinas em curso no calendário
 def CT_001():
-    calendarXPATH = '//*[@id="nav-drawer"]/ul/li[3]/a'
-    login()
+    try:
+      calendarXPATH = '//*[@id="nav-drawer"]/ul/li[3]/a'
+      login()
     
-    elem = driver.find_element(By.XPATH, calendarXPATH)
-    elem.click()
+      elem = driver.find_element(By.XPATH, calendarXPATH)
+      elem.click()
+      assert 'Av Qualis - Produto está marcado(a) para esta data' in driver.page_source
 
-    assert 'Av Qualis - Produto está marcado(a) para esta data' not in driver.page_source
+      print('CT_001: ✅ - Exibir datas de atividades das disciplinas em curso no calendário')
+
+    except:
+      print('CT_001: ❌ - Exibir datas de atividades das disciplinas em curso no calendário')
 
 
 if(__name__ == '__main__'):
     CT_001()
-    time.sleep(5)
+    time.sleep(3)
     driver.close()
 
 
