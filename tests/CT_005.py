77666ec5fa16df74a70777692a4b3e8d3d8ab7ff
Beatriz Salles <beatrizsallesss@gmail.com>
2022-08-29 22:58:45 -0300

    test: Create other tests

---
+++ tests/CT_005.py
@@ -0,0 +1,43 @@
+
+
+import time
+from login import login
+from utils import driver, UNDBClassURL
+from selenium.webdriver.common.by import By
+
+painelButtonXPath = '//*[@id="nav-drawer"]/ul/li[2]/a'
+filterButtonId= 'groupingdropdown'
+notStartedCoursesOptionXPath = '//*[@id="yui_3_17_2_1_1661822226367_181"]/ul/li[5]'
+
+
+# Exibir cursos não iniciados
+def CT_005():
+    try:
+      login()
+      driver.get(UNDBClassURL)
+
+      painelButton = driver.find_element(By.XPATH, painelButtonXPath)
+      painelButton.click()
+
+      filterButton = driver.find_element(By.ID, filterButtonId)
+      filterButton.click()
+
+      notStartedCoursesOption = driver.find_element(By.XPATH, notStartedCoursesOptionXPath)
+      notStartedCoursesOption.click()
+
+      assert 'Nenhum curso' not in driver.page_source
+      
+
+      print('CT_005: ✅ - Exibir cursos não iniciados')
+
+    except:
+      print('CT_005: ❌ - Exibir cursos não iniciados')
+
+
+if(__name__ == '__main__'):
+    start_time = time.time()
+    CT_005()
+    time.sleep(3)
+    driver.close()
+    print("Done in", round(time.time() - start_time, 2), "s.")
+
